e4dc94da63b54511886d1c92ddee6c56cf104bc8
RogueTurtle <66068126+RogueTurtle@users.noreply.github.com>
2022-12-03 16:39:52 +0000

    Day3 part1 solution

---
+++ src/main/java/aoc/Day3.java
@@ -0,0 +1,51 @@
+package aoc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Day3 {
+    String[] chars = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    public static void main(String[] args) {
+        Day3 d3 = new Day3();
+        System.out.println(d3.part1());
+    }
+
+    public Integer part1() {
+        Integer output = 0;
+        String line = "";
+        String commonChar = "";
+        
+        try (FileReader fr = new FileReader("input/Day3.csv"); BufferedReader br = new BufferedReader(fr)) {
+            while (br.ready()) {
+                line = br.readLine();
+                Integer length = line.length() /2;
+                String[] halves = {line.substring(0,length), line.substring(length)};
+                commonChar = findCommonChars(halves[0], halves[1]);
+                output += Arrays.asList(chars).indexOf(commonChar) + 1;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+    public String findCommonChars(String a, String b) {
+        StringBuilder resultBuilder = new StringBuilder();
+        Set<Character> charsMap = new HashSet<Character>();
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i); //a and b are the two words given by the user
+             if (b.indexOf(ch) != -1){
+                 charsMap.add(Character.valueOf(ch));
+             }
+        }
+
+        Iterator<Character> charsIterator = charsMap.iterator();
+        while(charsIterator.hasNext()) {
+            resultBuilder.append(charsIterator.next().charValue());
+        }
+        return resultBuilder.toString();
+    }
+}
